84f681d50a4773348c82bf52237578f6ac272b52
Priya Singh <priya18082004@gmail.com>
2026-09-19 11:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (65.74%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -3,8 +3,8 @@ public:
     int findxor(vector<int>& nums, int index, int xorsum){
         if(index==nums.size())return xorsum;
         int pick= findxor(nums, index+1, xorsum^nums[index]);
-        int nonpick= findxor(nums, index+1, xorsum);
-        return pick + nonpick;
+        int notpick= findxor(nums, index+1, xorsum);
+        return pick + notpick;
 
     }
     int subsetXORSum(vector<int>& nums) {
